200e14004acbd24b4ede477b56b4cb9e4d6e0367
Gabe Stein <gabe@ghscommunications.com>
2014-11-02 15:31:17 -0500

    header image changes

--- single.php
+++ single.php
@@ -2,9 +2,15 @@
 
 <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 
+<style>
+.article.single header:before {
+  background-image: url(<?php echo $imgsrc; ?>);
+}
+</style>
+
 <article class="single" id="<?php the_ID(); ?>">
     <?php $imgsrc = wp_get_attachment_url(get_post_thumbnail_id()); ?>
-    <header style="background-image: url(<?php echo $imgsrc; ?>);">
+    <header>
         <div class="intro-text" >
             <?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?>
             <h2><?php the_title(); ?></h2>
