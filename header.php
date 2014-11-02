63de406334fedeef9028f98ba28bc927b3095128
Gabe Stein <gabe@ghscommunications.com>
2014-11-02 14:53:33 -0500

    replace google analytics

--- header.php
+++ header.php
@@ -64,31 +64,20 @@
 	    <?php } ?>
 
         <!-- begin GOOGLE ANALYTICS -->
-        <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-20901984-1']);
-            _gaq.push(['_setDomainName', '.buquad.com']);
-            _gaq.push(['_trackPageview']);
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            (function() {
-                var ga = document.createElement('script');
-                ga.type = 'text/javascript';
-                ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
-        <script type="text/javascript">
-          function recordOutboundLink(link, category, action) {
-            _gat._getTrackerByName()._trackEvent(category, action);
-            setTimeout('document.location = "' + link.href + '"', 100);
-          }
+          ga('create', 'UA-6858151-9', 'auto');
+          ga('send', 'pageview');
+
         </script>
         <!-- end GOOGLE ANALYTICS -->
-        
+
         <?php wp_head(); ?>
-    
+
     </head>
     <body>
         <header>
