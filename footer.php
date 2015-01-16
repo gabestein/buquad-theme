cc5bc991633056bb1ff9d50b11ae421bc55c6f57
Gabe Stein <gabe@ghscommunications.com>
2015-01-15 20:09:40 -0500

    add cookie script

--- footer.php
+++ footer.php
@@ -7,9 +7,7 @@
         <script>
         (function($) {
           window.visits = $.cookie('visits') || 0;
-          if(window.visits === 0) {
-            $.cookie('visits', 1, { expires: 7300, path: '/' });
-          } else if(parseInt(window.visits) <= 6) {
+          if(parseInt(window.visits) <= 6) {
             window.visits = parseInt(window.visits) + 1;
             $.cookie('visits', window.visits, { expires: 7300, path: '/' });
           }
