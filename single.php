4eb06fc1e65d5d3adc63b094a042b203cde810a3
Gabe Stein <gabe@ghscommunications.com>
2015-01-17 15:54:39 -0500

    twitter

--- single.php
+++ single.php
@@ -45,7 +45,7 @@
         <div class="share-sub">
           <div class="share">
             <a class="facebook" onclick="fb_share('<?php the_permalink(); ?>')"><i class="fa fa-facebook"></i> Share</a>
-            <a class="twitter" onclick="twitter_share()" data-via="sleeperave" data-text="<?php echo get_the_excerpt(); ?>" data-url="<?php the_permalink(); ?>"><i class="fa fa-twitter"></i> Tweet</a>
+            <a class="twitter" onclick="twitter_share()"  data-via="sleeperave" data-text="<?php echo get_the_excerpt(); ?>" data-url="<?php the_permalink(); ?>"><i class="fa fa-twitter"></i> Tweet</a>
             <a class="email" target="_blank" href="mailto:?subject=Sleeper Ave.: <?php the_title(); ?>&body=Hi!%0D%0A%0D%0AI thought you might enjoy this Sleeper Ave. comic by Ed Stein.%0D%0A%0D%0A<?php htmlentities(the_title()); ?>%0D%0A<?php echo htmlentities(get_the_excerpt()); ?>%0D%0A%0D%0A<?php htmlentities(the_permalink()); ?>"><i class="fa fa-envelope"></i> Email a Friend</a>
             <a class="support"><i class="fa fa-credit-card"></i> Support</a>
           </div>
