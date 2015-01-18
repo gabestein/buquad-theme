d2002d077721383f9766d33fc4709b2acf7ab126
Gabe Stein <gabe@ghscommunications.com>
2015-01-17 19:28:02 -0500

    cookie logic

--- footer.php
+++ footer.php
@@ -4,14 +4,5 @@
         <?php wp_footer(); ?>
 
         <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer-menu' ) ); ?>
-        <script>
-        (function($) {
-          window.visits = $.cookie('visits') || 0;
-          if(parseInt(window.visits) <= 6) {
-            window.visits = parseInt(window.visits) + 1;
-            $.cookie('visits', window.visits, { expires: 7300, path: '/' });
-          }
-        })(jQuery);
-        </script>
     </body>
 </html>
