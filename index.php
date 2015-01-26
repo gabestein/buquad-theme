f68603d011a5990a16a2cdbb1d4e8716ba96c480
Gabe Stein <gabe@ghscommunications.com>
2015-01-26 15:31:21 -0600

    homepage

--- index.php
+++ index.php
@@ -3,7 +3,25 @@ query_posts('cat=1&posts_per_page=1&order=DESC'); ?>
 <?php get_header(); ?>
 
 <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
-  <article class="single blog" id="<?php the_ID(); ?>">
+  <section class="recommender top">
+    <?php
+
+    $prev_post = get_previous_post(true);
+    if($prev_post !== '') {
+      article_card($prev_post->ID, 'prev');
+    }
+
+    $next_post = get_next_post(true);
+    if($next_post !== '') {
+      article_card($next_post->ID, 'next');
+    }
+
+    //get our actions
+    action_unit();
+
+    ?>
+  </section>
+  <article class="single" id="<?php the_ID(); ?>">
     <?php $imgsrc = wp_get_attachment_url(get_post_thumbnail_id()); ?>
 
     <!--<style>
@@ -15,52 +33,33 @@ query_posts('cat=1&posts_per_page=1&order=DESC'); ?>
 <header>
   <div class="intro-text" >
     <?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?>
-    <h4><?php the_category(' '); ?></h4>
+    <h4><?php my_post_number(); ?></h4>
     <h2><?php the_title(); ?></h2>
-    <h3><?php the_date(); ?></h3>
   </div>
 </header>
 <section class="body">
+  <?php the_content('Read More'); ?>
+  <?php wp_link_pages(array('before' => '<div class="paginate">Page:', 'after' => '</div>', 'next_or_number' => 'number')); ?>
+</section>
+<footer>
   <?php $connected = new WP_Query( array(
     'connected_type' => 'posts_to_posts',
     'connected_items' => get_queried_object(),
     'nopaging' => true,
     ) );
-    if($connected->have_posts()) {
-      $show_after_p = 3;
-      $content = apply_filters('the_content', $post->post_content);
-      if(substr_count($content, '<p>') > $show_after_p)
-      {
-        $contents = explode("<p>", $content);
-        $p_count = 1;
-        foreach($contents as $content)
-        {
-          if($p_count == $show_after_p)
-          {
-            ?>
-            <section class="related blog">
-              <h3>Related Stories</h3>
-              <?php while($connected->have_posts()) : $connected->the_post(); ?>
-                <?php article_card(get_the_ID()); ?>
-              <?php endwhile; ?>
-            </section>
-            <?php
-            echo '<p class="alignleft">';
-            echo $content;
-          } else {
-            echo "<p>";
-            echo $content;
-          }
-          $p_count++;
-        }
-      }
+    if($connected->have_posts()) { ?>
+      <section class="related">
+        <h3>Sleeper Alley</h3>
+        <p>Thoughts and reactions to this story from the artist and readers.</p>
+        <ul>
+          <?php while($connected->have_posts()) : $connected->the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+          <?php endwhile; ?>
+        </ul>
+      </section>
+      <?php
       wp_reset_postdata();
-    } else {
-      the_content('Read More');
-    }?>
-    <?php wp_link_pages(array('before' => '<div class="paginate">Page:', 'after' => '</div>', 'next_or_number' => 'number')); ?>
-  </section>
-  <footer>
+    } ?>
     <section class="plea">
       <p class="comments"><strong>Thoughts, comments or questions about this story?</strong> Send me a note <a href="/feedback">here</a>, or via email at <a href="mailto:ed@sleeperave.com?subject=Comment on <?php the_title(); ?>" target="_blank">ed@sleeperave.com</a>. I publish my favorite comments weekly on my blog.</p>
       <div class="share-sub">
@@ -112,6 +111,25 @@ query_posts('cat=1&posts_per_page=1&order=DESC'); ?>
     </ul>
   </div>
 </article>
+
+<section class="recommender bottom">
+  <?php
+
+  $prev_post = get_previous_post(true);
+  if($prev_post !== '') {
+    article_card($prev_post->ID, 'prev');
+  }
+
+  $next_post = get_next_post(true);
+  if($next_post !== '') {
+    article_card($next_post->ID, 'next');
+  }
+
+  //get our actions
+  action_unit();
+
+  ?>
+</section>
 <?php } /* close while */ } else { ?>
 
   <article class="404">
@@ -121,5 +139,5 @@ query_posts('cat=1&posts_per_page=1&order=DESC'); ?>
 
   <?php } /* close if */ ?>
   <?php get_footer(); ?>
-
+  
 <?php wp_reset_query(); ?>
