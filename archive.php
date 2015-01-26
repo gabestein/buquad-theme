c503402c10e84a116d75443b7e30771089bd3042
Gabe Stein <gabe@ghscommunications.com>
2015-01-26 12:42:54 -0600

    sleeper alley

--- archive.php
+++ archive.php
@@ -15,10 +15,22 @@
   <?php if ( have_posts() ) { ?>
     <div class="card container">
     <?php while ( have_posts() ) {
-        the_post();
-        article_card(get_the_id());
-      }
-    ?>
+        the_post(); ?>
+        <article class="single blog archive" id="<?php the_ID(); ?>">
+          <header>
+            <div class="intro-text" >
+              <?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?>
+              <h4><?php the_category(' '); ?></h4>
+              <h2><?php the_title(); ?></h2>
+              <h3><?php the_date(); ?></h3>
+            </div>
+          </header>
+          <section class="body">
+            <?php the_excerpt(); ?>
+            <?php the_permalink(); ?>
+          </section>
+        </article>
+    <?php } ?>
     <nav class="pagination">
       <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
 
