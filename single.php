ec0d20cd8aa62b2b3dfd399c902967471ff278fb
Gabe Stein <gabe@ghscommunications.com>
2015-01-26 12:39:37 -0600

    sleeper alley

--- single.php
+++ single.php
@@ -13,6 +13,7 @@
     <header>
         <div class="intro-text" >
             <?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?>
+            <h4><?php the_category(' '); ?></h4>
             <h2><?php the_title(); ?></h2>
             <h3><?php the_date(); ?></h3>
         </div>
