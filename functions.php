91af1fb00a42721ff0ecc0b09f6f5a05b475e78a
Gabe Stein <gabe@ghscommunications.com>
2015-01-25 18:16:17 -0600

    email and social shortcode

--- functions.php
+++ functions.php
@@ -194,7 +194,7 @@ function email_subscribe_form() { ?>
   <iframe name="mailchimp_target" height=0 width=0 style="height: 0; width: 0;"></iframe>
 <?php }
 
-function subscribe_block() { ?>
+function subscribe_block() { return( ?>
   <div class="share-sub">
     <div class="subscribe">
       <h3>Subscribe to the Sleeper Ave. mailing list</h3>
@@ -205,11 +205,11 @@ function subscribe_block() { ?>
       <a href="https://twitter.com/sleeperave" class="twitter-follow-button" data-show-count="false" data-size="small">Follow @sleeperave</a>
     </div>
   </div>
-<?php }
+<?php )}
 
 function subscribe_block_shortcode(){
   $block = subscribe_block();
-  return '$block';
+  return $block;
 }
 add_shortcode( 'subscribe', 'subscribe_block_shortcode' );
 
